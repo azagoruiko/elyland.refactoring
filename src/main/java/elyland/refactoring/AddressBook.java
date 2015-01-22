b26fc192e490afab1ef68b25371c7c50414a8e25
a@zagoruiko.org.ua <a@zagoruiko.org.ua>
2015-01-22 19:40:37 +0200

    added javadoc comments

--- src/main/java/elyland/refactoring/AddressBook.java
+++ src/main/java/elyland/refactoring/AddressBook.java
@@ -12,12 +12,21 @@ public class AddressBook {
         this.db = db;
     }
 
+    /**
+     * Searches a person and checks if its number is mobile
+     * @param name a name to search
+     * @return true if person's number is mobile 
+     */
     public boolean hasMobile(String name) {
         Person person = db.findPerson(name);
         if (person == null) return false;
         return person.hasMobile();
     }
 
+    /**
+     * Retrieves person count
+     * @return person count
+     */
     public int getSize() {
         List<Person> people = db.getAll();
         return people.size();
@@ -26,6 +35,9 @@ public class AddressBook {
     /**
      * Gets the given user's mobile phone number, or null if he doesn't have
      * one.
+     * 
+     * @param name person name to search
+     * @return mobile phone number, or null if he doesn't have one.
      */
     public String getMobile(String name) {
         Person person = db.findPerson(name);
@@ -36,6 +48,9 @@ public class AddressBook {
 
     /**
      * Returns all names in the book truncated to the given length.
+     * 
+     * @param maxLength Maximum length for each returned name
+     * @return a list of strings that are names cut if was necessary
      */
     public List<String> getNames(int maxLength) {
         List<Person> people = db.getAll();
@@ -53,6 +68,8 @@ public class AddressBook {
 
     /**
      * Returns all people who have mobile phone numbers.
+     * 
+     * @return List of people who have mobile phone numbers.
      */
     public List<Person> getMobileList() {
         List<Person> people = db.getAll();
@@ -68,6 +85,12 @@ public class AddressBook {
         return filteredList;
     }
     
+    /**
+     * For testing purpose (adding a new person)
+     * @param name person name
+     * @param phoneNumber person phone number
+     * @return saved Person
+     */
     public Person addPerson(String name, String phoneNumber) {
         Person p = new Person(name, new PhoneNumber(phoneNumber));
         db.addPerson(p);
